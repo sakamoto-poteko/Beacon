ddd5ce9553d5331544e399b49ebdfa515ca3548c
Sakamoto Poteko <poteko@poteko.moe>
2020-11-26 23:45:38 +0800

    Get MSA bearer token

--- BeaconClient/Program.cs
+++ BeaconClient/Program.cs
@@ -3,33 +3,43 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Client;
 
 namespace Beacon
 {
     class Program
     {
+        public static IPublicClientApplication PublicClientApp;
+
         static async System.Threading.Tasks.Task Main(string[] args)
         {
-            var builder = new HostBuilder()
-                .ConfigureAppConfiguration((ctx, config) =>
-                {
-                    config.AddJsonFile("appsettings.json", optional: true);
-                    config.AddEnvironmentVariables();
+            var app = PublicClientApplicationBuilder.Create("4c975b25-5425-4c0c-a209-2690835c1260")
+                    .WithRedirectUri("http://localhost")
+                    .WithAuthority(AzureCloudInstance.AzurePublic, "9188040d-6c67-4c5b-b112-36a304b66dad")
+                    .Build();
 
-                    if (args != null)
+            var authResult = await app.AcquireTokenInteractive(new[] { "" }).ExecuteAsync();
+
+            var builder = new HostBuilder()
+                    .ConfigureAppConfiguration((ctx, config) =>
                     {
-                        config.AddCommandLine(args);
-                    }
-                })
-                .ConfigureServices((ctx, services) =>
-                {
-                    ConfigureServices(services);
-                })
-                .ConfigureLogging((ctx, loggingBuilder) =>
-                {
-                    loggingBuilder.AddConfiguration(ctx.Configuration.GetSection("Logging"));
-                    loggingBuilder.AddConsole();
-                });
+                        config.AddJsonFile("appsettings.json", optional: true);
+                        config.AddEnvironmentVariables();
+
+                        if (args != null)
+                        {
+                            config.AddCommandLine(args);
+                        }
+                    })
+                    .ConfigureServices((ctx, services) =>
+                    {
+                        ConfigureServices(services);
+                    })
+                    .ConfigureLogging((ctx, loggingBuilder) =>
+                    {
+                        loggingBuilder.AddConfiguration(ctx.Configuration.GetSection("Logging"));
+                        loggingBuilder.AddConsole();
+                    });
 
             await builder.RunConsoleAsync();
         }
